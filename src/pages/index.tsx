2354ea8fc50b4890c65eaca27637490736c6adf4
Ditlef <ditlef.diseth@schibsted.com>
2021-10-22 19:04:53 +0200

    Start off point

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,37 +8,8 @@ import ProductCard from '../components/product-card';
 import { Product } from '../totally-real-database/api';
 
 export default function App() {
-  const { data, error, mutate } = useSwr('/api/products');
-  const [productInModal, setProductInModal] = useState<Product | null>(null);
-
-  async function handleDeleteProduct(productId: string) {
-    await deleteProduct(productId);
-    mutate();
-  }
-
-  if (error) {
-    return (
-      <div>
-        <p>Something went wrong</p>
-      </div>
-    );
-  }
-
-  if (!data) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
   return (
     <div className={'page-container'}>
-      <EditProductModal
-        product={productInModal}
-        onDismiss={() => setProductInModal(null)}
-      />
-
       <div className="flex items-center justify-between	mt-40 mb-12">
         <h2>Here are some products!</h2>
         <Link href="/new-product">
@@ -49,21 +20,7 @@ export default function App() {
           </a>
         </Link>
       </div>
-      <ul className="grid f-grid grid-cols-1 md:grid-cols-3 md:gap-32">
-        {data.map((product, index) => {
-          return (
-            <ProductCard
-              key={product.id}
-              product={product}
-              index={index}
-              onClick={() =>
-                setProductInModal((prev) => (prev ? null : product))
-              }
-              handleDeleteProduct={handleDeleteProduct}
-            />
-          );
-        })}
-      </ul>
+      <ul className="grid f-grid grid-cols-1 md:grid-cols-3 md:gap-32"></ul>
     </div>
   );
 }
