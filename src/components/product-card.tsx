149452d547b5bdc6ac55ff3379a1d224bce27e1a
Benjamin Akar <benjaminakar2001@gmail.com>
2021-10-25 13:51:40 +0200

    Update remove button

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -35,7 +35,6 @@ export default function ProductCard({
           <Button
             data-cy={'Delete'}
             negative
-            pill
             small
             onClick={() => handleDeleteProduct(product.id)}
           >
